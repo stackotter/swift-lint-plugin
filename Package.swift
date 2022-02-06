7e647c84ddb066ee8e84800b1b2cc960ea6f59f3
stackotter <stackotter@stackotter.dev>
2022-02-06 14:30:23 +1000

    Remove commented out targets

--- Package.swift
+++ Package.swift
@@ -6,18 +6,12 @@ let package = Package(
     name: "SwiftLintPlugin",
     platforms: [.macOS(.v10_12)],
     products: [
-//        .plugin(name: "SwiftLintPlugin", targets: ["SwiftLintPlugin"]),
         .plugin(name: "LintCommand", targets: ["LintCommand"])
     ],
     dependencies: [
         .package(url: "https://github.com/stackotter/SwiftLint", from: "0.46.2")
     ],
     targets: [
-//        .plugin(
-//            name: "SwiftLintPlugin",
-//            capability: .buildTool(),
-//            dependencies: [.product(name: "swiftlint", package: "SwiftLint")]
-//        ),
         .plugin(
             name: "LintCommand",
             capability: .command(intent: .custom(
